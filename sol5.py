bacba77b6c4c14ad6802380ef9c4122138d3cea1
avichai <avichai.bendavid@mail.huji.ac.il>
2017-01-19 00:25:19 +0200

    model code

--- sol5.py
+++ sol5.py
@@ -3,10 +3,16 @@ import matplotlib.pyplot as plt
 from scipy.misc import imread as imread
 from skimage.color import rgb2gray
 import os
+from keras.models import Model
+from keras.layers import Convolution2D, Activation, Input, merge
 
 from scipy.ndimage.filters import convolve
 
 import sol5_utils
+
+FINALE_OUTPUT_CHANNELS = 1
+
+DEF_RES_BLOCKS = 5
 
 IM_NORM_FACTOR = 0.5
 
@@ -22,6 +28,8 @@ DIM_RGB = 3
 
 HEIGHT = 0
 WIDTH = 1
+DEF_KER_CONVE_HEIGHT = 3
+DEF_KER_CONVE_WIDTH = 3
 
 
 def read_image(filename, representation):
@@ -89,7 +97,7 @@ def load_dataset(filenames, batch_size, corruption_func, crop_size):
                     batch_filenames.append(cur_filename)
                     if cur_filename not in dic:
                         cur_image = read_image(cur_filename, 1)
-                        dic[cur_filename] = cur_filename
+                        dic[cur_filename] = cur_image
                     else:
                         cur_image = dic[cur_filename]
                     cur_corrupt_image = corruption_func(cur_image)
@@ -100,13 +108,49 @@ def load_dataset(filenames, batch_size, corruption_func, crop_size):
                                    :1][0]
                     start_width = np.random.permutation(cur_width - width)[:1][0]
 
-                    source_batch[counter, 0, ...] = cur_corrupt_image[
-                                                 start_height:start_height + height,
-                                                 start_width:start_width + width] - IM_NORM_FACTOR
+                    source_batch[counter, ...] = cur_corrupt_image[
+                                                    start_height:start_height + height,
+                                                    start_width:start_width + width] - IM_NORM_FACTOR
 
-                    target_batch[counter, 0, ...] = cur_image[
-                                                 start_height:start_height + height,
-                                                 start_width:start_width + width] - IM_NORM_FACTOR
+                    target_batch[counter, ...] = cur_image[
+                                                    start_height:start_height + height,
+                                                    start_width:start_width + width] - IM_NORM_FACTOR
                     counter += 1
             yield (source_batch.astype(np.float32), target_batch.astype(np.float32))
+
     return generator()
+
+
+def resblock(input_tensor, num_channels):
+    '''
+    creating a residual block
+    :param input_tensor: symbolic input tensor
+    :param num_channels: number of channels for each of its
+    convolutional layers
+    :return: a residual block
+    '''
+    conv1 = Convolution2D(num_channels, DEF_KER_CONVE_HEIGHT, DEF_KER_CONVE_WIDTH, border_mode='same')(input_tensor)
+    relu1 = Activation('relu')(conv1)
+    conv2 = Convolution2D(num_channels, DEF_KER_CONVE_HEIGHT, DEF_KER_CONVE_WIDTH, border_mode='same')(relu1)
+    return merge([input_tensor, conv2], mode='sum')
+
+
+def build_nn_model(height, width, num_channels):
+    '''
+    return an untrained Keras model
+    :param height: input dimension for model
+    :param width: input dimension for model
+    :param num_channels: output channels for all convolution layers except the very last
+    :return: an untrained Keras model
+    '''
+    input_tensor = Input(shape=(height, width, 1)) # todo change to: input_tensor = Input(shape=(1, height, width)) # after changing file mentioned in forum
+    conv1 = Convolution2D(num_channels, DEF_KER_CONVE_HEIGHT, DEF_KER_CONVE_WIDTH, border_mode='same')(input_tensor)
+    relu1 = Activation('relu')(conv1)
+    after_resblocks = relu1
+    for i in range(DEF_RES_BLOCKS):
+        after_resblocks = resblock(after_resblocks, num_channels)
+    last_addition = merge([relu1, after_resblocks], mode='sum')
+    last_conv = Convolution2D(FINALE_OUTPUT_CHANNELS, DEF_KER_CONVE_HEIGHT, DEF_KER_CONVE_WIDTH, border_mode='same')(last_addition)
+
+    model = Model(input=input_tensor, output=last_conv)
+    return model
--- testSol5.py
+++ testSol5.py
@@ -4,16 +4,19 @@ import sol5_utils
 import numpy as np
 import matplotlib.pyplot as plt
 
+DEF_NUM_CHANNELS = 10
+
 DEF_CROP_SIZE = (200, 200)
 
 DEF_BATCH_SIZE = 10
 
 
 def corruption_func(im):
-    noisy = im + 0.4 * np.std(im) * np.random.random(im.shape) #todo check if correct
+    noisy = im + 0.4 * np.std(im) * np.random.random(im.shape)  # todo check if correct
     return noisy
 
-def loadDataSet():
+
+def testLoadDataSet():
     filenames = sol5_utils.images_for_denoising()
     batch_size = DEF_BATCH_SIZE
     crop_size = DEF_CROP_SIZE
@@ -28,15 +31,26 @@ def loadDataSet():
     plt.imshow(target[0, 0, ...], cmap=plt.cm.gray)
     plt.show()
 
+# def testResBlock(): #todo still need to test
+#     output_tensor = sol5.resblock(input_tensor, num_channels)
 
 
+def test_build_nn_model():
+    height, width = DEF_CROP_SIZE
+    num_channels = DEF_NUM_CHANNELS
+    model = sol5.build_nn_model(height, width, num_channels)
 
-#tests
-# loadDataSet
+
+# tests
+# testLoadDataSet
+# testResBlock
+# test_build_nn_model
+
+tests = [test_build_nn_model]
 
 def main():
     try:
-        for test in [loadDataSet]:
+        for test in tests:
             test()
     except Exception as e:
         print('Failed due to: {0}'.format(e))
