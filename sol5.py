8957f27c982725b023290cb20bd483bb7197eeb7
avichai <avichai.bendavid@mail.huji.ac.il>
2017-01-20 01:26:39 +0200

    debluring denoising code

--- sol5.py
+++ sol5.py
@@ -1,13 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.misc import imread as imread
+from scipy.ndimage.filters import convolve
 from skimage.color import rgb2gray
 import os
 from keras.models import Model
 from keras.layers import Convolution2D, Activation, Input, merge
 from keras.optimizers import Adam
-
-from scipy.ndimage.filters import convolve
 
 import sol5_utils
 
@@ -31,6 +30,22 @@ HEIGHT = 0
 WIDTH = 1
 DEF_KER_CONVE_HEIGHT = 3
 DEF_KER_CONVE_WIDTH = 3
+
+DEF_CROP_SIZE_DENOISE = (24, 24)
+DEF_NUM_CHANNELS_DENOISE = 48
+DEF_BATCH_SIZE = 100
+DEF_SAMPLE_PER_EPOCH = 10000  # todo change
+DEF_NUM_EPOCHS_DENOISE = 5
+DEF_NUM_VALID_SAMPLES = 1000  # todo change
+
+DEF_MIN_SIGMA = 0.0
+DEF_MAX_SIGMA = 0.2
+
+DEF_CROP_SIZE_DEBLUR = (16, 16)
+DEF_NUM_CHANNELS_DEBLUR = 32
+DEF_NUM_EPOCHS_DEBLUR = 10
+
+DEF_KER_LIST = [7]
 
 
 def read_image(filename, representation):
@@ -105,17 +120,16 @@ def load_dataset(filenames, batch_size, corruption_func, crop_size):
                     cur_height = cur_image.shape[HEIGHT]
                     cur_width = cur_image.shape[WIDTH]
 
-                    start_height = np.random.permutation(cur_height - height)[
-                                   :1][0]
+                    start_height = np.random.permutation(cur_height - height)[:1][0]
                     start_width = np.random.permutation(cur_width - width)[:1][0]
 
                     source_batch[counter, ...] = cur_corrupt_image[
-                                                    start_height:start_height + height,
-                                                    start_width:start_width + width] - IM_NORM_FACTOR
+                                                 start_height:start_height + height,
+                                                 start_width:start_width + width] - IM_NORM_FACTOR
 
                     target_batch[counter, ...] = cur_image[
-                                                    start_height:start_height + height,
-                                                    start_width:start_width + width] - IM_NORM_FACTOR
+                                                 start_height:start_height + height,
+                                                 start_width:start_width + width] - IM_NORM_FACTOR
                     counter += 1
             yield (source_batch.astype(np.float32), target_batch.astype(np.float32))
 
@@ -144,15 +158,15 @@ def build_nn_model(height, width, num_channels):
     :param num_channels: output channels for all convolution layers except the very last
     :return: an untrained Keras model
     '''
-    # input_tensor = Input(shape=(1, height, width))
-    input_tensor = Input(shape=(height, width, 1)) # todo change to: input_tensor = Input(shape=(1, height, width)) # after changing file mentioned in forum
+    input_tensor = Input(shape=(1, height, width))
     conv1 = Convolution2D(num_channels, DEF_KER_CONVE_HEIGHT, DEF_KER_CONVE_WIDTH, border_mode='same')(input_tensor)
     relu1 = Activation('relu')(conv1)
     after_resblocks = relu1
     for i in range(DEF_RES_BLOCKS):
         after_resblocks = resblock(after_resblocks, num_channels)
     last_addition = merge([relu1, after_resblocks], mode='sum')
-    last_conv = Convolution2D(FINALE_OUTPUT_CHANNELS, DEF_KER_CONVE_HEIGHT, DEF_KER_CONVE_WIDTH, border_mode='same')(last_addition)
+    last_conv = Convolution2D(FINALE_OUTPUT_CHANNELS, DEF_KER_CONVE_HEIGHT, DEF_KER_CONVE_WIDTH, border_mode='same')(
+        last_addition)
 
     model = Model(input=input_tensor, output=last_conv)
     return model
@@ -174,8 +188,8 @@ def train_model(model, images, corruption_func, batch_size,
     '''
     num_images = len(images)
     inputShape = model.get_input_shape_at(0)
-    crop_size = (inputShape[1], inputShape[2]) # todo check if 1 and 2 is good
-    num_train_ims = int(0.8*num_images)
+    crop_size = (inputShape[-2], inputShape[-1])  # todo check if 1 and 2 is good
+    num_train_ims = int(0.8 * num_images)
 
     train_ims_names = images[:num_train_ims]
     test_ims_name = images[num_train_ims:]
@@ -186,3 +200,132 @@ def train_model(model, images, corruption_func, batch_size,
     model.compile(optimizer=Adam(beta_2=0.9), loss='mse')
     model.fit_generator(train_gen, samples_per_epoch, num_epochs,
                         validation_data=test_gen, nb_val_samples=num_valid_samples)
+
+
+def restore_image(corrupted_image, base_model, num_channels):
+    '''
+    return restored full images of any size
+    :param corrupted_image:  a grayscale image of shape (height, width) and with values in the [0, 1] range of
+    type float32
+    :param base_model: a neural network trained to restore small patches
+    :param num_channels: the number of channels used in the base model. Use it to construct the larger model
+    :return: restored full images of any size
+    '''
+    corrupted_image_shape = corrupted_image.shape
+    height, width = corrupted_image_shape[HEIGHT], corrupted_image_shape[WIDTH]
+
+    corrupted_image -= IM_NORM_FACTOR
+
+    full_im_model = build_nn_model(height, width, num_channels)
+    full_im_model.set_weights(base_model.get_weights())
+
+    restored_im = full_im_model.predict(corrupted_image[np.newaxis, np.newaxis, ...])[0][0, :, :]
+    return (restored_im + IM_NORM_FACTOR).clip(0, 1).astype(np.float32)
+
+
+def add_gaussian_noise(image, min_sigma, max_sigma):
+    '''
+    add random noise to image
+    :param image: a grayscale image with values in the [0, 1] range of type float32.
+    :param min_sigma: a non-negative scalar value representing the minimal variance of the gaussian distribution
+    :param max_sigma: a non-negative scalar value larger than or equal to min_sigma, representing the maximal
+    variance of the gaussian distribution
+    :return: the image with random noise
+    '''
+
+    sigma = np.random.uniform(min_sigma, max_sigma)
+    noisy = image + np.random.normal(loc=0.0, scale=sigma, size=image.shape)
+
+    return noisy.clip(0, 1).astype(np.float32)
+
+
+def learn_denoising_model(quick_mode=False):
+    '''
+    return a trained denoising model
+    :param quick_mode: If true, much lower values arguments for model
+    :return: a trained denoising model
+    '''
+    height, width = DEF_CROP_SIZE_DENOISE
+    num_channels = DEF_NUM_CHANNELS_DENOISE
+    images = sol5_utils.images_for_denoising()
+
+    if quick_mode == False:
+        batch_size = DEF_BATCH_SIZE
+        samples_per_epoch = DEF_SAMPLE_PER_EPOCH
+        num_epochs = DEF_NUM_EPOCHS_DENOISE
+        num_valid_samples = DEF_NUM_VALID_SAMPLES
+    else:
+        batch_size = 10
+        samples_per_epoch = 30
+        num_epochs = 2
+        num_valid_samples = 30
+
+    denoising_model = build_nn_model(height, width, num_channels)
+
+    if os.path.exists(sol5_utils.relpath('./model_den.h')):  # todo rm before submission
+        denoising_model.load_weights('./model_den.h')
+    else:
+        train_model(denoising_model, images, lambda im: add_gaussian_noise(im, DEF_MIN_SIGMA, DEF_MAX_SIGMA),
+                    batch_size,
+                    samples_per_epoch, num_epochs, num_valid_samples)
+        denoising_model.save_weights(sol5_utils.relpath('./model_den.h'))
+    return denoising_model, num_channels
+
+
+def add_motion_blur(image, kernel_size, angle):
+    '''
+    simulate motion blur on the given image
+    :param image: a grayscale image with values in the [0, 1] range of type float32.
+    :param kernel_size: an odd integer specifying the size of the kernel
+    :param angle:an angle in radians in the range [0, π)
+    :return: simulate motion blur on the given image
+    '''
+    kernel = sol5_utils.motion_blur_kernel(kernel_size, angle)
+    corrupt = convolve(image, kernel)
+    return corrupt.astype(np.float32)
+
+
+def random_motion_blur(image, list_of_kernel_sizes):
+    '''
+    simulate motion blur on the given image
+    :param image: a grayscale image with values in the [0, 1] range of type float32.
+    :param list_of_kernel_sizes:  a list of odd integers
+    :return: simulate motion blur on the given image
+    '''
+    num_kernels = len(list_of_kernel_sizes)
+    kernel_ind = np.random.permutation(num_kernels)
+    kernel_size = list_of_kernel_sizes[kernel_ind]
+    angle = np.random.uniform(0, np.pi)
+    return add_motion_blur(image, kernel_size, angle)
+
+
+def learn_deblurring_model(quick_mode=False):
+    '''
+    return a trained deblurring model, and the number of channels used in its construction
+    :param quick_mode: If true, much lower arguments for model
+    :return: a trained deblurring model, and the number of channels used in its construction
+    '''
+    height, width = DEF_CROP_SIZE_DEBLUR
+    num_channels = DEF_NUM_CHANNELS_DEBLUR
+    images = sol5_utils.images_for_deblurring()
+
+    if quick_mode == False:
+        batch_size = DEF_BATCH_SIZE
+        samples_per_epoch = DEF_SAMPLE_PER_EPOCH
+        num_epochs = DEF_NUM_EPOCHS_DEBLUR
+        num_valid_samples = DEF_NUM_VALID_SAMPLES
+    else:
+        batch_size = 10
+        samples_per_epoch = 30
+        num_epochs = 2
+        num_valid_samples = 30
+
+    debluring_model = build_nn_model(height, width, num_channels)
+
+    if os.path.exists(sol5_utils.relpath('./model_den.h')):  # todo rm before submission
+        debluring_model.load_weights('./model_deb.h')
+    else:
+        train_model(debluring_model, images, lambda im: random_motion_blur(im, DEF_KER_LIST), batch_size,
+                    samples_per_epoch, num_epochs, num_valid_samples)
+        debluring_model.save_weights(sol5_utils.relpath('./model_deb.h'))
+    return debluring_model, num_channels
--- testSol5.py
+++ testSol5.py
@@ -3,23 +3,40 @@ import sol5_utils
 
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-DEF_NUM_VALID_SMPLES = 20
+DEF_NUM_VALID_SAMPLES = 1000  # todo change
 
 DEF_NUM_EPOCHS = 5
 
-DEF_SAMPLE_PER_EPOCH = 100
+DEF_SAMPLE_PER_EPOCH = 10000  # todo change
 
-DEF_NUM_CHANNELS = 10
+DEF_NUM_CHANNELS = 48
 
-DEF_CROP_SIZE = (200, 200)
+DEF_CROP_SIZE = (24, 24)
 
-DEF_BATCH_SIZE = 10
+DEF_BATCH_SIZE = 100
 
 
 def corruption_func(im):
     noisy = im + 0.4 * np.std(im) * np.random.random(im.shape)  # todo check if correct
     return noisy
+
+
+def add_gaussian_noise(image):
+    '''
+    add random noise to image
+    :param image: a grayscale image with values in the [0, 1] range of type float32.
+    :param min_sigma: a non-negative scalar value representing the minimal variance of the gaussian distribution
+    :param max_sigma: a non-negative scalar value larger than or equal to min_sigma, representing the maximal
+    variance of the gaussian distribution
+    :return: the image with random noise
+    '''
+
+    sigma = np.random.uniform(0, 1)
+    noisy = image + np.random.normal(loc=0.0, scale=sigma)
+
+    return noisy.clip(0, 1).astype(np.float32)
 
 
 def testLoadDataSet():
@@ -36,6 +53,7 @@ def testLoadDataSet():
     plt.figure()
     plt.imshow(target[0, 0, ...], cmap=plt.cm.gray)
     plt.show()
+
 
 # def testResBlock(): #todo still need to test
 #     output_tensor = sol5.resblock(input_tensor, num_channels)
@@ -54,13 +72,117 @@ def test_train_model():
     images = sol5_utils.images_for_denoising()
     samples_per_epoch = DEF_SAMPLE_PER_EPOCH
     num_epochs = DEF_NUM_EPOCHS
-    num_valid_samples = DEF_NUM_VALID_SMPLES
+    num_valid_samples = DEF_NUM_VALID_SAMPLES
 
     model = sol5.build_nn_model(height, width, num_channels)
 
-    sol5.train_model(model, images, corruption_func, batch_size,
-                samples_per_epoch, num_epochs, num_valid_samples)
+    corruption_function = lambda im: sol5.add_gaussian_noise(im, 0.0, 0.2)
 
+    sol5.train_model(model, images, corruption_function, batch_size,
+                     samples_per_epoch, num_epochs, num_valid_samples)
+
+    model.save_weights(sol5_utils.relpath('./model.h4'), overwrite=False)
+
+
+def test_restored_image():
+    height, width = DEF_CROP_SIZE
+    num_channels = DEF_NUM_CHANNELS
+    images = sol5_utils.images_for_denoising()
+
+    base_model = sol5.build_nn_model(height, width, num_channels)
+    base_model.load_weights('./model_den.h')
+
+    # print(base_model.get_weights()[0][0][0][0])
+
+    im = sol5.read_image(images[2], 1)
+    corrupted_image = corruption_func(im)
+    restored_image = sol5.restore_image(corrupted_image, base_model, num_channels)
+
+    plt.figure()
+    plt.imshow(im, cmap=plt.cm.gray)
+    plt.figure()
+    plt.imshow(corrupted_image, cmap=plt.cm.gray)
+    plt.figure()
+    plt.imshow(restored_image, cmap=plt.cm.gray)
+    plt.show()
+
+
+def test_add_gaussian_noise():
+    images = sol5_utils.images_for_denoising()
+    im = sol5.read_image(images[2], 1)
+    corrupted_image = sol5.add_gaussian_noise(im, 0, 1)
+
+    plt.figure()
+    plt.imshow(im, cmap=plt.cm.gray)
+    plt.figure()
+    plt.imshow(corrupted_image, cmap=plt.cm.gray)
+    plt.show()
+
+
+def test_learn_denoising_model():
+    model, num_channels = sol5.learn_denoising_model(quick_mode=False)  # todo this one is needed
+    # model, num_channels = sol5.learn_denoising_model(quick_mode=True)
+
+    images = sol5_utils.images_for_denoising()
+    im = sol5.read_image(images[2], 1)
+    corrupted_image = sol5.add_gaussian_noise(im, 0, 0.2)
+    restored_image = sol5.restore_image(corrupted_image, model, num_channels)
+
+    plt.figure()
+    plt.imshow(im, cmap=plt.cm.gray)
+    plt.figure()
+    plt.imshow(corrupted_image, cmap=plt.cm.gray)
+    plt.figure()
+    plt.imshow(restored_image, cmap=plt.cm.gray)
+    plt.show()
+
+
+def test_add_motion_blur():
+    images = sol5_utils.images_for_denoising()
+    im = sol5.read_image(images[2], 1)
+
+    kernel_size = 3
+    angle = 2.2
+
+    corrupted = sol5.add_motion_blur(im, kernel_size, angle)
+
+    plt.figure()
+    plt.imshow(im, cmap=plt.cm.gray)
+    plt.figure()
+    plt.imshow(corrupted, cmap=plt.cm.gray)
+    plt.show()
+
+
+def test_random_motion_blur():
+    images = sol5_utils.images_for_denoising()
+    im = sol5.read_image(images[2], 1)
+
+    list_of_kernel_sizes = [3, 5, 7, 9]
+    corrupted = sol5.random_motion_blur(im, list_of_kernel_sizes)
+
+    plt.figure()
+    plt.imshow(im, cmap=plt.cm.gray)
+    plt.figure()
+    plt.imshow(corrupted, cmap=plt.cm.gray)
+    plt.show()
+
+
+def test_learn_deblurring_model():
+    # model, num_channels = sol5.learn_deblurring_model(quick_mode=False) # todo this one is needed
+    model, num_channels = sol5.learn_deblurring_model(quick_mode=True)
+
+    images = sol5_utils.images_for_deblurring()
+    im = sol5.read_image(images[2], 1)
+    corrupted_image = sol5.random_motion_blur(im, [7])
+    restored_image = sol5.restore_image(corrupted_image, model, num_channels)
+
+    plt.figure()
+    plt.imshow(im, cmap=plt.cm.gray)
+    plt.figure()
+    plt.imshow(corrupted_image, cmap=plt.cm.gray)
+    plt.figure()
+    plt.imshow(restored_image, cmap=plt.cm.gray)
+    plt.show()
 
 
 # tests
@@ -68,8 +190,14 @@ def test_train_model():
 # testResBlock
 # test_build_nn_model
 # test_train_model
+# test_restored_image
+# test_add_gaussian_noise
+# test_add_motion_blur
+# test_random_motion_blur
+# test_learn_deblurring_model
 
-tests = [test_train_model]
+tests = [test_learn_denoising_model]
+
 
 def main():
     try:
